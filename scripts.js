78116bb418ef44df8cb305c81f1fa29571a28391
URROVA (Ulises Z.) <arovaurrova@gmail.com>
2020-10-30 11:31:02 -0300

    Volvimos a la pagina modular: contenido y barra en archivos separados Esta vez no en iframes, sino que puse la libreria zepto (jquery chiquito) para que cargue navbar.html / navbar_es.html en las paginas. Tambien tienen un meta extra para el lenguage del navbar.
    Amend: estoy haciendo cagada tras cagada >:(

--- scripts.js
+++ scripts.js
@@ -1,8 +1,35 @@
+	
+
 //Funcion copiada y pegada de intArnet
 function getRandomInt(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+
+//Carga la barra de navegacion
+//Con zepto por que jquery es pesau y cash no tiene .load
+var metas = document.getElementsByTagName("meta");
+for (var i = 0; i < metas.length; i++){
+	var element = metas[i];
+	var meta_tipo = element.getAttribute("name");
+	//Si es un meta de idioma, se fija si esta en ingles o español
+	if (meta_tipo == "idioma"){
+		var idioma = element.getAttribute("content");
+		switch (idioma){
+			case "ingles":
+				$(function() {
+					$("#Navbar_loader").load("navbar.html");
+				});
+				break;
+			case "español":
+				$(function() {
+					$("#Navbar_loader").load("navbar_es.html");
+				});
+				break;
+		}
+	}
 }
 
 //Un miniscript que cambia el texto en el marquee
