d163219a8d47cd6a87177884a626dd6900b8c336
URROVA (Ulises Z.) <arovaurrova@gmail.com>
2021-06-26 15:48:32 -0300

    Añadido el fanart de veltarden. Muchas gracias :)

--- js/oc_picker.js
+++ js/oc_picker.js
@@ -1,7 +1,8 @@
 var OCs = new Array(
     "<a href=\"oc_evilution.html\"><img src=\"images/URROVA/URROVA 256.gif\" class=\"urrova1\" title=\"URROVA floating in the air . . .\"></a>",
+    "<a href=\"oc_evilution.html\"><img width=40% src=\"images/URROVA/4_urrova_Veltarden.png\" class=\"urrova1\" title=\"Thank you very much, veltarden!! :)\"></a>",
     "<a href=\"oc_evilution.html\"><img src=\"images/URROVA/URROVA tomando mate.png\" class=\"urrova2\"title=\"URROVA drinking mate -w- (mate emoji)\"></a>"
 )
 
 var oc_container = document.getElementById("oc_container");
-oc_container.innerHTML = OCs[getRandomInt(0,2)];
+oc_container.innerHTML = OCs[getRandomInt(0,OCs.length)];
