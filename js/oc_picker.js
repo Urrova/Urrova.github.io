d7eed9dd626da4e5ca4f32dd2077b5689cd39db4
URROVA (Ulises Z.) <arovaurrova@gmail.com>
2021-06-27 19:10:47 -0300

    Ajuste

--- js/oc_picker.js
+++ js/oc_picker.js
@@ -1,6 +1,6 @@
 var OCs = new Array(
     "<a href=\"oc_evilution.html\"><img src=\"images/URROVA/URROVA 256.gif\" class=\"urrova1\" title=\"URROVA floating in the air . . .\"></a>",
-    "<a href=\"oc_evilution.html\"><img width=40% src=\"images/URROVA/4_urrova_Veltarden.png\" class=\"urrova1\" title=\"Thank you very much, veltarden!! :)\"></a>",
+    "<a href=\"oc_evilution.html\"><img width=480px src=\"images/URROVA/4_urrova_Veltarden.png\" class=\"urrova1\" title=\"Thank you very much, veltarden!! :)\"></a>",
     "<a href=\"oc_evilution.html\"><img src=\"images/URROVA/URROVA tomando mate.png\" class=\"urrova2\"title=\"URROVA drinking mate -w- (mate emoji)\"></a>"
 )
 
